8303401e7a6c1b62d4b2fa4eb99c19329a172e97
MorrisJansen <morris8jansen@hotmail.com>
2024-07-15 12:05:33 +0200

    wijzeging pad voor gh-pages

--- src/router.js
+++ src/router.js
@@ -18,7 +18,7 @@ export default new Router({
       props: { ...campagneStartData },
     },
     {
-      path: "/Airco/vraag1",
+      path: "vraag1",
       component: vraag1,
       props: {...campagneStartData},
     },
