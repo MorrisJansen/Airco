4c8e55a8178ba8d6434a8d15db10342a1c460e98
MorrisJansen <morris8jansen@hotmail.com>
2024-07-19 16:13:06 +0200

    toch nog wat ruimtes aangepast aan vraag 1

--- src/components/vraag1.vue
+++ src/components/vraag1.vue
@@ -376,14 +376,16 @@ export default {
 
   .pijl-vraag1
     zoom: 300%
-    margin-bottom: 25px
+
 
   .vraag-hoeveel
     margin-left: 3rem!important
     zoom: 210%
     display: block
     padding: 10px 0
-    margin-top: 25px
+    margin-top: 15px
+    position: relative
+    right:  10px
 
   .vraag
     margin-left: 3rem!important
